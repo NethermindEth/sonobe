7b8b88b507cb970e199bae84fc0acf436cbaf4b7
Fantoni0 <antoniomlf@protonmail.com>
2025-04-17 10:51:47 +0200

    Add bench for comparing MSMs

--- benches/commitment_schemes.rs
+++ benches/commitment_schemes.rs
@@ -1,3 +1,4 @@
+use ark_ec::VariableBaseMSM;
 use ark_pallas::{Fr, Projective};
 use ark_std::{log2, UniformRand, Zero};
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
@@ -10,7 +11,7 @@ use rand::{Rng, RngCore};
 use std::time::Duration;
 
 // Matrix sizes to test (dimensions will be 2^size x 2^size)
-const MATRIX_SIZES: &[usize] = &[4, 6, 8]; // 16x16, 64x64, 256x256 matrices
+const MATRIX_SIZES: &[usize] = &[4, 6, 8, 10]; // 16, 64, 128, 1024
 const SPARSITY_FACTOR: f64 = 0.05; // 5% non-zero elements for sparse matrices
 
 /// Generates a random dense matrix of specified dimensions
@@ -95,6 +96,48 @@ fn bench_sparse_commits(c: &mut Criterion) {
 
     group.finish();
 }
+/// Benches the same MSM in 2 scenarios: a single MSM, or multiple smaller MSM until reaching the same size
+fn bench_msm(c: &mut Criterion) {
+    let mut group = c.benchmark_group("MultiScalar Exponentiations");
+    group.measurement_time(Duration::from_secs(10));
+    let mut rng = ark_std::test_rng();
 
-criterion_group!(benches, bench_dense_commits, bench_sparse_commits,);
+    for &size in MATRIX_SIZES {
+        let n = 1 << size; // 2^size
+        let matrix = random_dense_matrix::<Projective>(n, &mut rng);
+        let data = matrix.as_dense_slice().unwrap();
+        let partial_data: Vec<_> = (0..n).map(|_| Fr::rand(&mut rng)).collect();
+
+        // Use fully qualified type syntax for Affine
+        type ProjectiveAffine = <Projective as ark_ec::CurveGroup>::Affine;
+
+        // Setup parameters
+        let generators: Vec<ProjectiveAffine> =
+            (0..n).map(|_| ProjectiveAffine::rand(&mut rng)).collect();
+        let partial_gen = &generators[..n];
+
+        let pedersen_params = Pedersen::<Projective>::setup_prover(&mut rng, n * n).unwrap();
+
+        group.bench_with_input(BenchmarkId::new("Single Large MSM", n), &n, |b, _| {
+            b.iter(|| Projective::msm_unchecked(&pedersen_params.generators, data));
+        });
+
+        group.bench_with_input(BenchmarkId::new("Many Small MSM", n), &n, |b, _| {
+            b.iter(|| {
+                // Do n MSM os size n instead of a large MSM osf size n*n.
+                for _ in 0..n {
+                    let _ = Projective::msm_unchecked(&partial_gen, &partial_data);
+                }
+            });
+        });
+    }
+    group.finish();
+}
+
+criterion_group!(
+    benches,
+    bench_msm,
+    bench_dense_commits,
+    bench_sparse_commits
+);
 criterion_main!(benches);
