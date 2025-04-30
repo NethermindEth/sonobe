0faa893a247a47dc2ed7ce906ebf8b59fb43593f
Fantoni0 <antoniomlf@protonmail.com>
2025-04-30 15:27:14 +0200

    Change trait impl

--- folding-schemes/src/curve_impls.rs
+++ folding-schemes/src/curve_impls.rs
@@ -1,22 +1,11 @@
-// This module contains explicit implementations of the Curve trait
-// for common Arkworks curves to make them available to external users.
+// This module contains utility functions for common Arkworks curves
+// to make them easier to use with the Curve trait.
 
-use crate::Curve;
-use ark_ec::short_weierstrass::{Projective, SWCurveConfig};
-use ark_r1cs_std::{
-    fields::fp::FpVar,
-    groups::curves::short_weierstrass::ProjectiveVar,
-};
-use ark_std::Zero;
+use ark_ec::short_weierstrass::Projective;
 
 #[cfg(feature = "pallas")]
 pub mod pallas {
-    use super::*;
-    use ark_pallas::{Projective as PallasProjective, Config as PallasConfig};
-    
-    impl Curve for PallasProjective {
-        type Var = ProjectiveVar<PallasConfig, FpVar<ark_pallas::Fq>>;
-    }
+    use ark_pallas::Projective as PallasProjective;
     
     pub fn pallas_curve() -> PallasProjective {
         PallasProjective::zero()
@@ -25,12 +14,7 @@ pub mod pallas {
 
 #[cfg(feature = "vesta")]
 pub mod vesta {
-    use super::*;
-    use ark_vesta::{Projective as VestaProjective, Config as VestaConfig};
-    
-    impl Curve for VestaProjective {
-        type Var = ProjectiveVar<VestaConfig, FpVar<ark_vesta::Fq>>;
-    }
+    use ark_vesta::Projective as VestaProjective;
     
     pub fn vesta_curve() -> VestaProjective {
         VestaProjective::zero()
@@ -39,14 +23,9 @@ pub mod vesta {
 
 #[cfg(feature = "bn254")]
 pub mod bn254 {
-    use super::*;
-    use ark_bn254::{G1Projective, Config as BN254Parameters};
-    
-    impl Curve for G1Projective {
-        type Var = ProjectiveVar<BN254Parameters, FpVar<ark_bn254::Fq>>;
-    }
+    use ark_bn254::G1Projective;
     
     pub fn bn254_curve() -> G1Projective {
         G1Projective::zero()
     }
-} 
+}
