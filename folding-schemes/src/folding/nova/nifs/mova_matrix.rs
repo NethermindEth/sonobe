53e3f6d8e06f44e7b1f3419f26e4aaa387058285
Fantoni0 <antoniomlf@protonmail.com>
2025-06-09 12:27:51 +0200

    Update mova_matrix tests

--- folding-schemes/src/folding/nova/nifs/mova_matrix.rs
+++ folding-schemes/src/folding/nova/nifs/mova_matrix.rs
@@ -524,7 +524,7 @@ pub mod tests {
 
         // Set up transcript and commitment scheme
         let hyrax_params =
-            HyraxGenerators::<Projective>::setup(&mut rng, log2(mat_dim * mat_dim) as usize);
+            HyraxGenerators::<Projective>::setup(&mut rng, mat_dim * mat_dim as usize);
         let poseidon_config = poseidon_canonical_config::<Fr>();
         let mut transcript_p = PoseidonSponge::<Fr>::new(&poseidon_config);
         let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
@@ -574,7 +574,7 @@ pub mod tests {
 
         // Set up transcript and commitment scheme
         let hyrax_params =
-            HyraxGenerators::<Projective>::setup(&mut rng, log2(mat_dim * mat_dim) as usize);
+            HyraxGenerators::<Projective>::setup(&mut rng, mat_dim * mat_dim);
         let poseidon_config = poseidon_canonical_config::<Fr>();
         let mut transcript_p = PoseidonSponge::<Fr>::new(&poseidon_config);
         let mut transcript_v = PoseidonSponge::<Fr>::new(&poseidon_config);
